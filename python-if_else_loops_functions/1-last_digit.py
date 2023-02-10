f468e63b308e9995896763ba0ff68d197bb3e36b
Samuel <s.olubode@alustudent.com>
2023-02-10 08:37:58 -0800

    Find last digit or random ints

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+firstStr = 'Last digit of'
+isStr = 'is'
+lastDigit = number % 10
+if lastDigit > 5:
+    finalStr = 'and is greater than 5'
+elif lastDigit == 0:
+    finalStr = 'and is 0'
+elif lastDigit < 6 and lastDigit != 0:
+    finalStr = 'and is less than 6 and not 0'
+print(firstStr, number, isStr, lastDigit, finalStr)
