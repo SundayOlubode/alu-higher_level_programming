6a157f0b469ec13a0e1b959c7bd6ebd305b01987
Samuel <s.olubode@alustudent.com>
2023-02-28 12:56:54 -0800

    Update script

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-list1 = [num for num in range(0, 14, 2)]
-
 def print_list_integer(mylist):
-    for i in mylist:
-        print('{}'.format(mylist[i]))
+    for num in mylist:
+        print('{:d}'.format(mylist[i]))
