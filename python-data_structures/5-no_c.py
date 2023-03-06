e7b81563501b975b3ecb2702ae88dc562fb85318
Samuel <s.olubode@alustudent.com>
2023-03-05 23:04:58 -0800

    Remove 'c' and 'C' from string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    for letter in my_string:
-        if letter == 'c' or letter == 'C':
-            my_string.remove(letter)
+    my_string = my_string.translate({ord('C'): ''})
+    my_string = my_string.translate({ord('c'): ''})
     return my_string
