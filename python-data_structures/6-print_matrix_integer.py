35447f2c67e4f6e7ded2f97dc6278c4e31d74214
Samuel <s.olubode@alustudent.com>
2023-03-06 00:05:57 -0800

    Update: print items

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for sublist in matrix:
         if len(sublist) > 0:
             for item in sublist:
-                if item == sublist[-1]
+                if item == sublist[-1]:
                     print('{:d}'.format(item))
                 else:
                     print('{:d}'.format(item, end=' '))
