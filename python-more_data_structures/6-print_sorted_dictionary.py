dfb80a999505bee5c776134536441adae3dd5937
Samuel <s.olubode@alustudent.com>
2023-03-08 03:28:28 -0800

    Update script

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@
 
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary)
-    for k, v in sorted_dict:
+    for k, v in sorted_dict.items:
         print('{}: {}'.format(k, v))
