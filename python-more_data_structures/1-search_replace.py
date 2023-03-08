3bac99ad1284e1e0331051d42b8fee1f6f1bddcc
Samuel <s.olubode@alustudent.com>
2023-03-08 02:37:39 -0800

    Update script

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
     def replace_search(x):
         if x == search:
             return replace
-    new_list = list(map(replace_search, mylist))
+    new_list = list(map(replace_search, my_list))
     return new_list
