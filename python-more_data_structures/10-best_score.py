12663bc307435bd09f102cabf3bba9a1eb25a458
Samuel <s.olubode@alustudent.com>
2023-03-08 04:40:26 -0800

    Get best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 
 
 def best_score(a_dictionary):
-    if len(a_dictionary) < 1:
+    if len(a_dictionary) < 1 or a_dictionary is None:
         return None
     return max(a_dictionary, key=a_dictionary.get)
