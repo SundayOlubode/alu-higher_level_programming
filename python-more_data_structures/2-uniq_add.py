f17bdb41d64cacb2700112225fe0d547a1ca20ee
Samuel <s.olubode@alustudent.com>
2023-03-08 02:46:32 -0800

    Update script

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from functools import reduce
 
 
 def uniq_add(my_list=[]):
-    res = reduce(lambda x, y: x + y, set(my_list))
-    return res
+    res = set(my_list)
+    Sum = sum(res)
+    return Sum
