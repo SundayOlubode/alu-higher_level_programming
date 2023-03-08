3aa4191d18c0ee64b1ba5215c8ea6c02d8ad0476
Samuel <s.olubode@alustudent.com>
2023-03-08 03:15:43 -0800

    Update script

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 
-def only_diff_elements(set_1, set_2):
-    set_items = list(set_1) + list(set_2)
-    diff_elems = set(set_items)
-    return diff_elems
+def only_diff_elements(set_1, set_2): 
+    return set_1 ^ set_2
