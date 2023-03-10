da3e450fc7ea9a8d715a6c4eab25205aae18bb4f
Samuel <s.olubode@alustudent.com>
2023-03-10 04:26:07 -0800

    Debug: add some salt

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,9 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print('{:d}'.format(my_list[i]))
+            print('{:d}'.format(my_list[i]), end='')
+        except IndexError:
+            raise
         except (TypeError, ValueError):
             continue
         else:
