9196b29608568ef8da5cd3e103b74ce187d187bf
Samuel <s.olubode@alustudent.com>
2023-03-10 05:50:22 -0800

    check if it works

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -12,8 +12,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print('division by 0')
         except IndexError:
             print('out of range')
-        except:
-            result = 0
         else:
             new_list.append(result)
         finally:
