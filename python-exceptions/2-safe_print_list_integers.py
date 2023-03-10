ec478c008e29c6169167f7b2a447c46136ff3ccc
Samuel <s.olubode@alustudent.com>
2023-03-10 04:20:39 -0800

    Print first x int elements

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    if x == 0:
+        print()
+        return 0
+
+    counter = 0
+
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]))
+        except (TypeError, ValueError):
+            continue
+        else:
+            counter += 1
+    print()
+    return counter
