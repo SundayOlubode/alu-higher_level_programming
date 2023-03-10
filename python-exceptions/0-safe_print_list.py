21d44b1e099bf6cc1f0b6063adebb082415513b0
Samuel <s.olubode@alustudent.com>
2023-03-10 03:18:04 -0800

    Error and exception

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    for item in my_list:
+        try:
+            print(item, end='')
+        except IndexError:
+            return
+        else:
+            counter += 1
+    return counter
