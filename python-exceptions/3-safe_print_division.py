d13b5ba9bd0e0ead8c49d0c667c384fa69e6b790
Samuel <s.olubode@alustudent.com>
2023-03-10 05:19:04 -0800

    Debug: obey PEP8

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,8 +3,9 @@
 
 def safe_print_division(a, b):
     try:
-        result = round((a / b),1)
+        result = round((a / b), 1)
     except:
         result = None
     finally:
         print('Inside result: {}'.format(result))
+        return result
