51b201cd640b94786cc2814eed72e754e9068955
Samuel <s.olubode@alustudent.com>
2023-03-10 03:40:00 -0800

    Update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    for item in my_list:
+    if x == 0:
+        return counter
+
+    for i in range(x):
         try:
-            print(item, end='')
+            print(my_list[i], end='')
         except IndexError:
             return
         else:
