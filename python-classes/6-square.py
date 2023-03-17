50efaa884ffa1196ee5544b4a8c2c712218dbe29
Samuel <s.olubode@alustudent.com>
2023-03-17 11:20:09 -0700

    set position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square():
     ' Define with optionla args and setters '
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0,0)):
         self.size = size
         self.position = position
 
