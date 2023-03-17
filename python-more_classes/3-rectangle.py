3b4ab8b539ea7aaf72fce7b133141ca74dfa667b
Samuel <s.olubode@alustudent.com>
2023-03-17 13:16:15 -0700

    finish the job

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -45,5 +45,8 @@ class Rectangle:
     def __str__(self):
         if self.__height == 0 or self.__width == 0:
             return ''
+        rec = ''
         for i in range(self.__height):
-            print('#' * self.__width)
+            rec += '#' * self.__width
+            rec += '\n'
+        return rec
