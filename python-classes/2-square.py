f84b58157d9960d7fdc6057e34d869778dafaba9
Samuel <s.olubode@alustudent.com>
2023-03-17 09:33:03 -0700

    Set properties

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Define a class of Square """
+
+
+class Square():
+    ' Define with optionla args and setters '
+    def __init__(self, size=0):
+        self.ourAtt = size
+
+    @property
+    def ourAtt(self):
+        return self.__size
+    
+    @size.setter
+    def ourAtt(self, size):
+        if (not int(size)):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
