44e5ad8b6466c45884ab2ca895d9de5c76de780e
Samuel <s.olubode@alustudent.com>
2023-03-17 13:00:33 -0700

    Let's have something to print

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -41,3 +41,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return print()
+        for i in range(self.__height):
+            print('#' * self.__width)
