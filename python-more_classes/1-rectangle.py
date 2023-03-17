8287b23135bf1f6fb0c09bee310976c662016c06
Samuel <s.olubode@alustudent.com>
2023-03-17 12:42:36 -0700

    modify

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError('width must be >= 0')
         else:
-            self.__width = width
+            self.__width = value
 
     @property
     def height(self):
@@ -32,4 +32,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError('height must be >= 0')
         else:
-            self.__height = height
+            self.__height = value
