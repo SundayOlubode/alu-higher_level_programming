57154036d5c8add4516fa354a2c864047c4cb1fc
Samuel <s.olubode@alustudent.com>
2023-03-22 10:58:51 -0700

    List subarray sort

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Write a class MyList that inherits from list: """
+
+
+class MyList(list):
+    """ Sorts lists object """
+
+    def print_sorted(self):
+        self.sort()
+        return self
