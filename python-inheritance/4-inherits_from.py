8cd0a6a4d4e56dda4bb844a36567953d1edc07f5
Samuel <s.olubode@alustudent.com>
2023-03-23 09:23:13 -0700

    check if inherited

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Check if only a subclass """
+
+
+def inherits_from(obj, a_class):
+    """ check inheritance """
+    return isinstance(obj, a_class) and type(obj) not a_class
