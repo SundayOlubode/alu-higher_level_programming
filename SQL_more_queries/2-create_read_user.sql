f8dc6a158091ad8b19dede688c253f0a8c598876
Samuel <s.olubode@alustudent.com>
2023-03-30 13:35:05 -0700

    Create U and DB

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Create user 2
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2@localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON htbn_0d_02 TO 'user_0d_2@localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON htbn_0d_02 TO 'user_0d_2'@'localhost';
