0adabf46ed79fba591ca12d364c73015d29d71da
Samuel <s.olubode@alustudent.com>
2023-03-30 13:18:11 -0700

    Create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Root user
 
-CREATE USER user_0d_1@localhost IF NOT EXISTS IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
