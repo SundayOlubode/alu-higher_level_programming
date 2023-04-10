a115f715b534df6f6a5efb6584cb08357a9825cf
ssamuel <Samuel>
2023-04-10 10:50:28 -0700

    Add test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,8 +12,6 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
-    if a + 1 = a:
-        raise Overflow('int too large to convert to float')
 
     if type(a) is float:
         a = int(a)
