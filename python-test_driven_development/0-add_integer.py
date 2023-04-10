66d3d016d4dbefc291eb52b2a340ac21e3aea34e
Samuel <63305676+SundayOlubode@users.noreply.github.com>
2023-04-10 19:49:17 +0100

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,7 +6,6 @@
 
 def add_integer(a, b=98):
     """This function adds two integers"""
-    
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
