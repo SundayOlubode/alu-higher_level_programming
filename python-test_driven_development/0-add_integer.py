342fb87b4f272ae0904253576a07ff18500a2952
ssamuel <Samuel>
2023-04-10 10:48:46 -0700

    Add test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
     This module create a function that adds two integers
 """
 
+
 def add_integer(a, b=98):
     """ 
     This function adds two integers
@@ -11,7 +12,9 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
-    
+    if a + 1 = a:
+        raise Overflow('int too large to convert to float')
+
     if type(a) is float:
         a = int(a)
     if type(b) is float:
