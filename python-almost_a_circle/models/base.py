ebbca956a3b6bac9bd0afbc092f7a1b8f50f0610
Samuel <s.olubode@alustudent.com>
2023-04-20 03:23:33 -0700

    adjust

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,8 +7,8 @@ class Base():
 
     def __init__(self, id=None):
         """ Init """
-        if id not None:
-            self.id = id
-        else:
+        if id is None:
             __nb_objects =+ 1
             self.id = __nb_objects
+        else:
+            self.id = id
