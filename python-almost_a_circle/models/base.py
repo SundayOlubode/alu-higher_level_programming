974dec4db3f44e2da5aa1d51e953d7ebd102e582
Samuel <s.olubode@alustudent.com>
2023-04-20 05:25:05 -0700

    instances

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -10,17 +10,17 @@ class Base():
         """ Init """
         self.id = id
 
-        @property
-        def id(self):
-            return self.__id
+    @property
+    def id(self):
+        return self.__id
 
-        @id.setter
-        def id(self, val):
-            if val is None:
-                Base.__nb_objects += 1
-                self.__id = self.__nb_objects
-            else:
-                self.__id = val
+    @id.setter
+    def id(self, id):
+        if id is None:
+            Base.__nb_objects += 1
+            self.__id = Base.__nb_objects
+        else:
+            self.__id = id
 
 b1 = Base()
 b2 = Base()
