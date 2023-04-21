0db6ba52ebc5f19820a576d4ac916ae932cd8a8c
Samuel <s.olubode@alustudent.com>
2023-04-21 03:32:03 -0700

    Add area

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -64,3 +64,8 @@ class Rectangle(Base):
         if val < 0:
             raise ValueError("y must be >= 0")
         self.__y = val
+
+    def area(self):
+        """ compute area """
+        rec_area = self.__width * self.__height
+        return rec_area
