cfd6abeb4585cb7bcf536b5065b83e6515c5b0c5
Samuel <s.olubode@alustudent.com>
2023-04-21 02:34:41 -0700

    rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" rectangle.py """
+
+
+from base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle Class """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize every instance """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def setter(self, val):
+        self.__width = self.width
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, val):
+        self.__height = self.height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, val):
+        self.__x = self.x
+    
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, val):
+        self.__y = self.y
