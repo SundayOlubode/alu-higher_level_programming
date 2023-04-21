f20124d3cf0bc75ea469901502d7cffc3f191c1c
Samuel <s.olubode@alustudent.com>
2023-04-21 03:57:19 -0700

    Add class info

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,4 +85,4 @@ class Rectangle(Base):
     def __str__(self):
         """ class info """
         return '[Rectangle] ({}) {}/{} - {}/{}' \
-                .format(self.id, self.x, self.y, self.__width, self.__height)
+            .format(self.id, self.x, self.y, self.__width, self.__height)
