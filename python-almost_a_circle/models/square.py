104d2157603b700f085bffa108738ed8b3d060b9
Samuel <s.olubode@alustudent.com>
2023-04-24 00:28:38 -0700

    Class Square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" square.py """
+
+from models.rectangle import Rectangle
+
+class square(Rectangle):
+    """ square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ initialize instances """
+        super().__init__(id, width=size, height=size, x, y)
+
+
+    def __str__(self):
+        """ info """
+        return '[Square] ({}) {}/{} - {}'\
+                .format(self.id, self.x, self.y, self.width)
+
