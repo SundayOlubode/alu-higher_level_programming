7ef6c3abb5d3e3b61b783fca42063090a754138b
Samuel <s.olubode@alustudent.com>
2023-04-24 01:16:30 -0700

    List to json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,3 +21,11 @@ class Base():
             self.__id = self.__nb_objects
         else:
             self.__id = val
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Doc"""
+        if list_dictionaries is None or \
+                len(list_dictionaries) == 0:
+            return "[]"
+    return json.dumps(list_dictionaries)
