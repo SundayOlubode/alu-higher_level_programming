b19bf2e27ae1a6db99dab64454b7bfe03727e210
Samuel <s.olubode@alustudent.com>
2023-04-24 02:11:05 -0700

    Try something

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -31,3 +31,18 @@ class Base():
                 len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save json rep to a file """
+        
+        objs_dict_rep = []
+
+        with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
+            if list_objs is None or len(list_objs) < 1:
+                f.write("[]")
+                return
+
+            for obj in list_objs:
+                objs_dict_rep.append(obj.to_dictionary())
+            f.write(cls.to_json_string(objs_dict_rep))
