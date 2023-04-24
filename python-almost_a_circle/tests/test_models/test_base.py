b3f940a8cbc963b1081fa249527f26955cf324b3
Samuel <s.olubode@alustudent.com>
2023-04-24 14:46:34 -0700

    Basic testcase

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" test_base.py """
+
+
+from unittest import TestCase
+from models.base import Base
+
+
+class TestBase(TestCase):
+    """ Test Base class """
+    def test_base(self):
+        base_1 = Base()
+        base_2 = Base()
+        base_89 = Base(89)
+        base_3 = Base()
+        self.assertEqual(1, base_1.id)
+        self.assertEqual(2, base_2.id)
+        self.assertEqual(89, base_89.id)
+        self.assertEqual(3, base_3.id)
