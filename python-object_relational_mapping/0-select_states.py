9ed0d98d9d82374bc70516d1a808c8f058ce34d3
ssamuel <s.olubode@alustudent.com>
2023-04-26 05:27:36 -0700

    select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,26 +8,25 @@ import sys
 
 def connect_db():
     """ Connect db with MySQLdb """
-     db = MySQLdb.connect(
-             host='localhost',
-             port=3306
-             user=sys.argv[1],
-             passwd=sys.argv[2],
-             db=sys.argv[3]
-             )
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
 
-     cur = db.cursor()
+    cur = db.cursor()
 
-     cur.execute('SELECT * FROM states ORDER BY id')
+    cur.execute("SELECT * FROM states ORDER BY id")
 
-     rows = cur.fetchall()
+    rows = cur.fetchall()
 
-     for row in rows:
-         print(row)
+    for row in rows:
+        print(row)
 
-     cur.close()
-
-     db.close()
+    cur.close()
+    db.close()
 
 if __name__ == "__main__":
     connect_db()
