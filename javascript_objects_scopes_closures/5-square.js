93fdb497e1c3b16bf0003d0472749e2d6c193f3d
ssamuel <s.olubode@alustudent.com>
2023-06-09 05:38:32 -0700

    5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-import Rectangle from './4-rectangle.js';
+const Rectangle = require('./4-rectangle.js';)
 
 class Square extends Rectangle {
   constructor (size) {
@@ -8,4 +8,4 @@ class Square extends Rectangle {
   }
 }
 
-export default Square;
+module.exports = Square;
