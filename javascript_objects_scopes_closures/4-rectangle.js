eb9ecc4348cebb9e3dbe9a3a9f739e98528cb06e
ssamuel <s.olubode@alustudent.com>
2023-06-09 05:23:03 -0700

    4-rectangle.js

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    for (let i = 0; i < this.width; i++) {
+      console.log('X'.repeat(this.height));
+    }
+  }
+
+  double () {
+    for (let i = 0; i < this.height * 2; i++) {
+      console.log('X'.repeat(this.width * 2));
+    }
+  }
+}
+
+module.exports = Rectangle;
